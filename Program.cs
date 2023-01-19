f5505dbaa04527d35860b62f05aa1fa7cbfbfc95
ChimK <chim.kalunta@gmail.com>
2023-01-19 14:10:17 +0000

    Console .cs file

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Services;
+using CsvHelper;
+using System.Globalization;
+
+try
+{
+    var ps = new PowerService();
+
+    IEnumerable<PowerTrade> pts = ps.GetTrades(DateTime.Now);
+
+    using var writer = new StreamWriter("Load.csv");
+    using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+    {
+        Console.WriteLine("Generating Data File...");
+
+        foreach (var pt in pts)
+        {
+            csv.WriteRecords(pt.Periods);
+        }
+        Console.WriteLine("Done");
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
